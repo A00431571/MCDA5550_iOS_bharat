2316e153d99f14f25637ac90fa5524d6ffdba420
Bharat Bhushan Verma <43525253+A00431571@users.noreply.github.com>
2019-04-11 09:39:20 -0300

    iOS App Assignment

---
+++ GetGoingAppA00431571/GetGoingClass/Services/LocationService.swift
@@ -0,0 +1,55 @@
+//  LocationService.swift
+//  GetGoingClass
+//
+//  Created by Bharat Bhushan Verma on 2019-03-31.
+//  Copyright © 2019 SMU. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationService: NSObject {
+
+    // singleton
+    static let shared = LocationService()
+
+    // MARK: - Properties
+
+    var locationManager: CLLocationManager?
+    weak var delegate: LocationServiceDelegate?
+
+    private override init() {
+        super.init()
+
+        locationManager = CLLocationManager()
+
+        if CLLocationManager.authorizationStatus() == .notDetermined {
+            locationManager?.requestWhenInUseAuthorization()
+        }
+        locationManager?.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+        locationManager?.delegate = self
+    }
+
+    // MARK: - Location Manager
+
+    func startUpdatingLocation() {
+        locationManager?.startUpdatingLocation()
+    }
+
+    func stopUpdatingLocation() {
+        locationManager?.stopUpdatingLocation()
+    }
+}
+
+extension LocationService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        delegate?.didUpdateLocation(location: location)
+    }
+}
+
+// Custom protocol
+protocol LocationServiceDelegate: class {
+    func didUpdateLocation(location: CLLocation)
+}
+
